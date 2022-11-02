9c273a1c5fdf2ad34a0eb619d6bbf4c9c224a2f3
onkar <onkar271998@gmail.com>
2022-11-02 17:00:13 +0530

    Git stat added

--- src/Pages/Gitstat.jsx
+++ src/Pages/Gitstat.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import styles from "../styles/gitstat.module.css";
 import GitHubCalendar from "react-github-calendar"
+import {Stack} from "@chakra-ui/react"
 function Gitstat() {
   return (
     <div className={styles.container}>
     <div className={styles.title} >Github Stats</div>
     <GitHubCalendar username="Onkar271998" className={styles.calender}/>
+
+    <Stack mt={5} align={"center"} >
+          <img
+            align="center"
+            src="https://github-readme-stats.vercel.app/api?username=Onkar271998&show_icons=true&include_all_commits=true&count_private=true&hide=issues,contribs&border_radius=10&locale=en"
+            alt="OnkarDeshmukh"
+            height="139"
+          />
+          <img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=Onkar271998&border_radius=10;" alt="OnkarDeshmukh" />
+        </Stack>
     </div>
   )
 }
